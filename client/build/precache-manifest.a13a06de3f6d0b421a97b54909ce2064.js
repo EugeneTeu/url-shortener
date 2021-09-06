ef60e5d1b5acb043d92f2e7390191b6c7508abe9
eugene teu <eugeneteu@gmail.com>
2021-09-06 23:55:48 +0800

    add client build

---
+++ client/build/precache-manifest.a13a06de3f6d0b421a97b54909ce2064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfe484de90cefd6166d854dac4bffc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "861c340d0dd1c1dde816",
+    "url": "/static/css/2.10043171.chunk.css"
+  },
+  {
+    "revision": "fb717539389c405ad658",
+    "url": "/static/css/main.685f9217.chunk.css"
+  },
+  {
+    "revision": "861c340d0dd1c1dde816",
+    "url": "/static/js/2.b051f3ee.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.b051f3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb717539389c405ad658",
+    "url": "/static/js/main.370d5da0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
